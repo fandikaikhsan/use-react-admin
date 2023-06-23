a14aed1193b4603133bea08bb102cbc0367edae0
Fandika <fandika.ikhsan@gmail.com>
2023-06-23 18:07:23 +0700

    feat: add auth provider

--- src/admin/App.jsx
+++ src/admin/App.jsx
@@ -1,16 +1,35 @@
 // in src/admin/App.jsx
-import * as React from "react";
-import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import * as React from "react"
+import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin"
+import jsonServerProvider from "ra-data-json-server"
+import { LoginPage } from "./LoginPage"
+import { AuthProvider } from "./AuthProvider"
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+// const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com")
+const dataProvider = jsonServerProvider("http://localhost:8002")
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} edit={EditGuesser} recordRepresentation="name" />
-    <Resource name="posts" list={ListGuesser} edit={EditGuesser} recordRepresentation="title" />
-    <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
-  </Admin>
-);
+  // https://jsonplaceholder.typicode.com/tab?_end=10&_order=ASC&_sort=id&_start=0
 
-export default App;
+  <Admin
+    loginPage={LoginPage}
+    dataProvider={dataProvider}
+    authProvider={AuthProvider}
+  >
+    <Resource
+      name="product"
+      list={ListGuesser}
+      edit={EditGuesser}
+      recordRepresentation="name"
+    />
+    {/* <Resource
+      name="posts"
+      list={ListGuesser}
+      edit={EditGuesser}
+      recordRepresentation="title"
+    />
+    <Resource name="comments" list={ListGuesser} edit={EditGuesser} /> */}
+  </Admin>
+)
+
+export default App
